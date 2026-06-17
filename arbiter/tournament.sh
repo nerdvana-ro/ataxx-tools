f05ccf7be79faf8bd0b488fe25754dcc06068c6e
Cătălin Frâncu <cata@francu.com>
2026-06-17 17:07:33 +0300

    Actualizează scriptul de turneu.

--- arbiter/tournament.sh
+++ arbiter/tournament.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
+# Rulează acest fișier ca arbiter/tournament.sh din directorul ataxx-tools/
+# (rădăcina repoului).
+
 SAVE_DIR=~/Desktop/ataxx-games
 
 rm -rf $SAVE_DIR
 mkdir $SAVE_DIR
 
-php tournament.php \
-    --binary ../agent/doofus/build/doofus --name doofus1 \
-    --binary ../agent/doofus/build/doofus --name doofus2 \
+php arbiter/tournament.php \
+    --binary agent/doofus/build/doofus --name doofus1 \
+    --binary agent/doofus/build/doofus --name doofus2 \
     --rounds 1 \
     --save $SAVE_DIR \
     --save-inputs
